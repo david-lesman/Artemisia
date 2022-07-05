7652193c5da16d04f6b769c297669f460bb71384
JetKnightAlpha <yourifriedman@gmail.com>
2022-07-05 17:19:24 +0200

    Login page start

--- project/web/views.py
+++ project/web/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "web/index.html")
+def login(request):
+    return render(request, "web/login.html")
